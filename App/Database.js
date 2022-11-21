2cb8c6a6f6eb0931eb87507396d1001a64899643
Shinizle <irvankharismafihan@gmail.com>
2022-11-21 23:12:59 +0700

    Put DB Credentials to .env

--- App/Database.js
+++ App/Database.js
@@ -1,7 +1,9 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
+dotenv.config()
 
-const  db = new Sequelize('time_capsule', 'root', '', {
-    host: "localhost",
+const  db = new Sequelize(process.env.DB_TABLE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
 });
 
